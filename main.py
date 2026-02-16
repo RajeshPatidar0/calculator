9823a959d5e2e8ba94a35b7077be8fca8447a275
RajeshPatidar0 <166990110+RajeshPatidar0@users.noreply.github.com>
2026-02-16 15:38:20 +0530

    main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"message": "Calculator API Working 🚀"}
+
+
+@app.get("/add")
+def add(a: float, b: float):
+    return {"result": a + b}
+
+
+@app.get("/subtract")
+def subtract(a: float, b: float):
+    return {"result": a - b}
+
+
+@app.get("/multiply")
+def multiply(a: float, b: float):
+    return {"result": a * b}
+
+
+@app.get("/divide")
+def divide(a: float, b: float):
+    if b == 0:
+        return {"error": "Division by zero not allowed"}
+    return {"result": a / b}
